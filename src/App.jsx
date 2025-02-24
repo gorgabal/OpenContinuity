fdaa559ab35bab876cbde482fd7a401d696b9822
Koen Schoute <koen@gorgabal.nl>
2025-02-24 15:46:02 +0100

    WIP testing connection rxdb and react

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,69 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { addRxPlugin, createRxDatabase } from 'rxdb/plugins/core';
+import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
+import { getRxStorageDexie } from 'rxdb/plugins/storage-dexie';
+import { wrappedValidateAjvStorage } from 'rxdb/plugins/validate-ajv';
+import { RxDBUpdatePlugin } from 'rxdb/plugins/update';
+
+let myDocument;
+
+async function databaseSetup() {
+  addRxPlugin(RxDBDevModePlugin);
+  addRxPlugin(RxDBUpdatePlugin);
+
+
+  const myDatabase = await createRxDatabase({
+    name: 'mydatabase',
+    storage: wrappedValidateAjvStorage({
+      storage: getRxStorageDexie()
+    }),
+    ignoreDuplicate: true //FIXME: this should be set to false in production
+  });
+
+  await myDatabase.addCollections({
+    // name of the collection
+    count: {
+      // we use the JSON-schema standard
+      schema: {
+        version: 0,
+        primaryKey: 'id',
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            maxLength: 100 // <- the primary key must have maxLength
+          },
+          count: {
+            type: 'integer'
+          },
+        },
+        required: ['id', 'count']
+      }
+    }
+  });
+
+  myDocument = await myDatabase.count.insertIfNotExists({ id: '1', count: 0 });
+
+
+}
+
+function incrementCount() {
+  myDocument.incrementalUpdate({
+    $inc: {
+      count: 1
+    }
+  })
+
+  console.log(myDocument.getLatest().count);
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  databaseSetup().catch(console.error);
+
+  const [myDocument.count, setCount] = useState(0)
 
   return (
     <>
@@ -18,7 +79,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={incrementCount}>
           count is {count}
         </button>
         <p>
